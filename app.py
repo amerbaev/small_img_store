f79bbf24df153f63d5855f54399fae00deeb50a8
Arseny Amerbaev <amerbaev@gmail.com>
2017-11-18 13:53:37 +0300

    удален весь код, связанный с mongo, генерация в hdf5

--- app.py
+++ app.py
@@ -1,32 +1,11 @@
 # encoding utf-8
 
-import os
-from multiprocessing import Pool
-
-
-from pymongo import MongoClient
-import gridfs
-
-
-# fsbuck = gridfs.GridFSBucket(db)
-
-
-def get_file(name):
-    db = MongoClient().small_img
-    fs = gridfs.GridFS(db)
-    return fs.get_last_version(name).md5
-
-
-def get_files():
-    p = Pool(8)
-    test = p.map(get_file, [filename for filename in os.listdir('png')])
-    print(test)
+import h5py
+import numpy as np
 
 
 if __name__ == '__main__':
-    db = MongoClient().small_img
-    fs = gridfs.GridFS(db)
-    hashes = []
-    for grid_out in fs.find():
-        print(grid_out.metadata)
-    # print(len(hashes))
+    f = h5py.File('data/images.hdf5', 'r')
+    image_files = f['/images/img']
+
+    print(image_files[0].tobytes())
--- image_generator.py
+++ image_generator.py
@@ -1,22 +1,27 @@
 import io
-from multiprocessing import Pool
-
 import numpy as np
 from PIL import Image
-from pymongo import MongoClient
-from gridfs import GridFS
+import xxhash
+import h5py
+from tqdm import tqdm
+import sys
 
 
-def generate_img_to_gridfs(name: str) -> str:
-    image_bio = io.BytesIO()
-    image_array = np.random.rand(128, 128, 3) * 255
-    image = Image.fromarray(image_array.astype('uint8')).convert('RGBA')
-    image.save(image_bio, format='png')
-    fs = GridFS(MongoClient().small_img)
-    file_id = fs.put(image_bio.getvalue(), filename=name + '.png')
-    return str(file_id)
+def generate_img_to_hdf5(count: int):
+    images = []
+    hashes = []
+    for i in tqdm(range(count)):
+        image_bio = io.BytesIO()
+        image_array = np.random.rand(128, 128, 4) * 255
+        image = Image.fromarray(image_array.astype('uint8')).convert('RGBA')
+        image.save(image_bio, format='png')
+        images.append(image_bio.getvalue())
+        hashes.append(xxhash.xxh64(image_bio.getvalue()).digest())
+    with h5py.File('data/images.hdf5', 'w') as f:
+        img_group = f.create_group('images')
+        img_group.create_dataset('img', data=np.void(np.array(images)))
+        img_group.create_dataset('xxhash64', data=np.void(np.array(hashes)))
 
 
 if __name__ == '__main__':
-    with Pool(8) as p:
-        print(p.map(generate_img_to_gridfs, [str(n) for n in range(1000)]))
+    generate_img_to_hdf5(1000)
