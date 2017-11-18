93df9aad4175fceb1a05e130eb545504250a3b10
Arseny Amerbaev <amerbaev@gmail.com>
2017-11-18 14:04:03 +0300

    поиск по хешу

--- app.py
+++ app.py
@@ -4,8 +4,18 @@ import h5py
 import numpy as np
 
 
+def find_in_hdf5(images_group, xxhash) -> bytes:
+    index = np.where(images_group['xxhash64'].value == xxhash)
+    if index[0]:
+        return images_group['img'][index[0][0]].tobytes()
+    else:
+        return b''
+
+
 if __name__ == '__main__':
     f = h5py.File('data/images.hdf5', 'r')
-    image_files = f['/images/img']
+    images = f['/images']
 
-    print(image_files[0].tobytes())
+    xxhash = images['xxhash64'][30]
+    print(find_in_hdf5(images, np.void(xxhash)))
+    print(find_in_hdf5(images, np.void(b'notexsit')))
