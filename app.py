9abebad28b0453f4906177dc0163c19ebfb2e283
Arseny Amerbaev <amerbaev@gmail.com>
2017-11-21 11:26:23 +0300

    Параллельная генерация картинок. Размерности указаны явно

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 import h5py
 import numpy as np
+from image_generator import generate_img_hash
 
 
 def find_in_hdf5(images_group, xxhash) -> bytes:
@@ -12,10 +13,25 @@ def find_in_hdf5(images_group, xxhash) -> bytes:
         return b''
 
 
-if __name__ == '__main__':
-    f = h5py.File('data/images.hdf5', 'r')
-    images = f['/images']
+def init_hdf5(to_size):
+    f = h5py.File('data/images.hdf5')
+    images = f.create_group('images')
+    image_arr, hash_arr = generate_img_hash(10)
+    image_dset = images.create_dataset('img', data=image_arr, chunks=(100,), maxshape=(None,), dtype=image_arr.dtype)
+    hash_dset = images.create_dataset('xxhash64', data=hash_arr, chunks=(1000,), maxshape=(None,), dtype=hash_arr.dtype)
+    image_dset.resize(to_size, axis=0)
+    hash_dset.resize(to_size, axis=0)
+    f.close()
 
-    xxhash = images['xxhash64'][30]
-    print(find_in_hdf5(images, np.void(xxhash)))
-    print(find_in_hdf5(images, np.void(b'notexsit')))
+
+if __name__ == '__main__':
+    init_hdf5(1000000)
+
+    # f = h5py.File('data/images.hdf5', 'r+')
+    # image_dset = f['/images/img']
+    # hash_dset = f['/images/xxhash64']
+    #
+    # with open('png/test.png', 'wb') as png_file:
+    #     png_file.write(image_dset[1500])
+    # f.close()
+
--- image_generator.py
+++ image_generator.py
@@ -4,24 +4,45 @@ from PIL import Image
 import xxhash
 import h5py
 from tqdm import tqdm
-import sys
+from multiprocessing import Pool
+
+PROC = 8
+
+COUNT = 1000000
+BLOCK = 1000
 
 
-def generate_img_to_hdf5(count: int):
-    images = []
-    hashes = []
-    for i in tqdm(range(count)):
+def generate_img_hash(count):
+    images = np.empty(count, dtype=np.dtype((np.void, 49353)))
+    hashes = np.empty(count, dtype=np.dtype((np.void, 8)))
+    for i in range(count):
         image_bio = io.BytesIO()
-        image_array = np.random.rand(128, 128, 4) * 255
-        image = Image.fromarray(image_array.astype('uint8')).convert('RGBA')
+        image_array = np.random.randint(256, size=(128, 128, 3))
+        image = Image.fromarray(image_array.astype('uint8')).convert('RGB')
         image.save(image_bio, format='png')
-        images.append(image_bio.getvalue())
-        hashes.append(xxhash.xxh64(image_bio.getvalue()).digest())
-    with h5py.File('data/images.hdf5', 'w') as f:
-        img_group = f.create_group('images')
-        img_group.create_dataset('img', data=np.void(np.array(images)), dtype=np.dtype('V65790'))
-        img_group.create_dataset('xxhash64', data=np.void(np.array(hashes)), dtype=np.dtype('V8'))
+        images[i] = np.void(image_bio.getvalue())
+        hashes[i] = np.void(xxhash.xxh64(image_bio.getvalue()).digest())
+
+    return images, hashes
 
 
 if __name__ == '__main__':
-    generate_img_to_hdf5(1000)
+    gen_iterations = COUNT // (PROC * BLOCK)
+    p = Pool(PROC)
+
+    for i in tqdm(range(gen_iterations)):
+        gen_img = p.map(generate_img_hash, [BLOCK for _ in range(PROC)])
+        shift = i * BLOCK * PROC
+        f = h5py.File('data/images.hdf5', 'r+')
+        image_dset = f['/images/img']
+        hash_dset = f['/images/xxhash64']
+        for j, (images_arr, hashes_arr) in enumerate(gen_img):
+            for k in range(len(images_arr)):
+                total_index = shift + j * BLOCK + k
+                try:
+                    image_dset[total_index] = images_arr[k]
+                    hash_dset[total_index] = hashes_arr[k]
+                except Exception as e:
+                    f.close()
+                    print(e)
+        f.close()
