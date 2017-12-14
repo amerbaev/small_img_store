0afc25b62d35ce23c7668abef9a7ac23085e81b7
Arseny Amerbaev <amerbaev@gmail.com>
2017-12-14 14:49:11 +0300

    Проверка работы с учетом загрузки хешей в память

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from hdf5_store import Hdf5Store
 
 if __name__ == '__main__':
     file = 'data/images.hdf5'
+
+    print('Создание класса')
     store = Hdf5Store(file)
     img_number = 10000000
     # print('Создание файла')
@@ -17,11 +19,14 @@ if __name__ == '__main__':
     # print('Генерация изображений')
     # img_gen.generate_img_with_pool(img_number, store)
 
-    print('Время поиска хеша:', np.mean([timeit('store.find_index_by_hash(xxhash)',
-                                                setup='xxhash = store.get_hash_by_index(np.random.randint(img_number))',
-                                                globals=globals(),
-                                                number=1)
-                                         for _ in range(100)]))
+    print('Загрузка хешей в память')
+    store.load_hashes()
+    print('Хеши успешно загружены')
+    print('Время поиска хеша:', "{0:.8f}".format(np.mean([timeit('store.find_index_by_hash(xxhash)',
+                                                                 setup='xxhash = store.get_hash_by_index(np.random.randint(img_number))',
+                                                                 globals=globals(),
+                                                                 number=1)
+                                                          for _ in range(10000)])))
     print('Коллизии: ', store.find_collisions())
 
     store.close()
